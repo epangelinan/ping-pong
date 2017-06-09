5066a3410799c89dfcedc76fde6441b8b9e4db84
Esvethlynna Pangelinan <esveth@aol.com>
2017-06-09 12:33:23 -0700

    remove the values of the pingPongNumbers array after displaying their values in the html

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,23 @@
 $(document).ready(function() {
   $("form#leap-year").submit(function(event) {
     event.preventDefault();
+    $(".indexValue").remove();
     var numberInput = parseInt($("input#numberInput").val());
     pingPong(numberInput);
     pingPongNumbers.forEach(function(pingPongNumber) {
-      $(".results").append("<li>" + pingPongNumber + "</li>");
+      $(".results").append("<li class = 'indexValue'>" + pingPongNumber + "</li>");
     });
+
+    for (index = 0; index < numberInput; index++){
+      pingPongNumbers.pop();
+    }
   });
 });
 
 var pingPongNumbers = [];
 
 var pingPong = function(numberInput) {
-  alert(numberInput);
+  //alert(numberInput);
   for (index = 1; index <= numberInput; index++) {
     if (index % 15 === 0) {
       pingPongNumbers.push("ping-pong");
